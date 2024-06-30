9dc0d5b1c9a5c711209131981d0fa0ee73509b1d
Rocky Latchman <kidmongoose@gmail.com>
2024-06-30 10:09:14 -0700

    Initial commit

---
+++ password_manager.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import sqlite3
+import enum
+
+
+class PasswordManager:
+    def generate_password():
+        pass
+    
+    def save_account_credentials():
+        pass
+
+    def view_accounts():
+        pass
+
+    def remove_account():
+        pass
+
+
+class Database:
+    def connect():
+        pass
+
+class User:
+    def create_account():
+        pass
+
+    def signin():
+        pass
+
+    def signout():
+        pass
+    
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
