2aa97ea327d87c835077c821382aef532c56c18f
Rocky Latchman <kidmongoose@gmail.com>
2024-07-08 21:27:41 -0700

    Account creation implemented

--- password_manager.py
+++ password_manager.py
@@ -2,6 +2,7 @@
 import sqlite3
 from enum import Enum
 import random
+from passlib.hash import pbkdf2_sha256
 
 
 class PasswordManager:
@@ -22,11 +23,30 @@ class PasswordManager:
 
 class Database:
     def connect():
+        sql_connection = sqlite3.connect('user.db')
+        sql_cursor = sql_connection.cursor()
+        return [sql_connection, sql_cursor]
+
+    def create_table():
+        sql_cursor = Database.connect()
+        table = """CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR(150) NOT NULL, 
+        password VARCHAR(200) NOT NULL);""" 
+        sql_cursor[1].execute(table)
+   
+    def get_data():
         pass
 
+    def insert_data(username, password):
+        sql_cursor = Database.connect()
+        Database.create_table()
+        sql_cursor[1].execute("""INSERT INTO user(username, password) VALUES(?, ?)""", (username, password))
+        sql_cursor[0].commit()
+        sql_cursor[0].close()
+
 class User:
-    def create_account():
-        pass
+    def create_account(username, password):
+        Database.insert_data(username, pbkdf2_sha256.hash(password))
+        
 
     def signin():
         pass
@@ -57,7 +77,14 @@ def main():
     if userInput == 1:
        pass
     elif userInput == 2:
-       pass 
+        print("\n")
+        username = input('Enter your username: ')
+        print("\n")
+        password = input('Enter your password: ')
+        User.create_account(username, password)
+        print("\n")
+        print("Account created")
+        print("\n")
     elif userInput == 3:
        print("\n")
        print(f'Here is your password: {PasswordManager.generate_password()}')
